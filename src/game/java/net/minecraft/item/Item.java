1cacc9f7368f4ee6ea66b3721bc0c51d73cc402d
flowdev-yee <32jwells@stu.asher.k12.ok.us>
2026-05-05 11:40:07 -0500

    Update Item.java

--- src/game/java/net/minecraft/item/Item.java
+++ src/game/java/net/minecraft/item/Item.java
@@ -1166,7 +1166,7 @@ public class Item {
 
 	public static enum ToolMaterial {
 		WOOD(0, 59, 2.0F, 0.0F, 15), STONE(1, 131, 4.0F, 1.0F, 5), IRON(2, 250, 6.0F, 2.0F, 14),
-		DIAMOND(3, 1561, 8.0F, 3.0F, 10), GOLD(0, 32, 12.0F, 0.0F, 22);
+		DIAMOND(3, 1561, 8.0F, 3.0F, 10), NETHERITE(4, 2031, 9.0F, 4.0F, 15), GOLD(0, 32, 12.0F, 0.0F, 22);
 
 		private final int harvestLevel;
 		private final int maxUses;
